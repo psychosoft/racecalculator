d0b592ee5390dd345c8f174a44253a985580b058
Robert Wallén <robert.wallen@sinch.com>
2023-06-09 18:02:21 +0200

    mixed class criteria and skipping teams that does not qualify

--- src/org/brosenius/racecalculator/RaceCalculatorUtil.java
+++ src/org/brosenius/racecalculator/RaceCalculatorUtil.java
@@ -4,7 +4,6 @@ import org.brosenius.domain.Gender;
 import org.brosenius.domain.Race;
 import org.brosenius.domain.compare.RaceTimeComparator;
 import org.brosenius.domain.compare.TeamResultComparator;
-import org.brosenius.exception.CompetitionCriteriaException;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -33,7 +32,7 @@ public class RaceCalculatorUtil {
         Vector<Vector<Race>> teamsWithBestRaces = new Vector(new Vector());
 
         racesGroupedByTeamID.keySet().stream().forEach(teamID -> {
-            Vector<Race> bestRaces = selectBestRacesForTeamIdIncludeMinimumOneFemale(teamID,numTopRidersPerTeam,listOfIndividualResults);
+            Vector<Race> bestRaces = selectBestMixedRacesForTeamId(teamID,numTopRidersPerTeam,listOfIndividualResults);
             teamsWithBestRaces.add(bestRaces);
         });
 
@@ -46,7 +45,8 @@ public class RaceCalculatorUtil {
                                 System.out.println("Team:" + race.getTeamID() + " Rider:" + race.getNameID() + " Gender:" + classGenderMap.get(race.getClassID()) + " Time:" + race.getTotal());
                             }
                     );
-                    System.out.println("Team total time:" + teamTotal);
+            //only print teams that has total time
+            if(teamTotal.get() > 0) System.out.println("Team total time:" + teamTotal);
                 }
         );
 
@@ -54,24 +54,44 @@ public class RaceCalculatorUtil {
 
     }
 
-    public static Vector<Race> selectBestRacesForTeamIdIncludeMinimumOneFemale(int teamId,int numRacesToInclude, Vector<Race> races){
+    private static long numGender(Vector<Race> races, Gender gender) {
+        return races.stream().filter(r -> classGenderMap.get(r.getClassID()) == gender).count();
+    }
+
+    private static Gender getMissingGender (Vector<Race> races) {
+        return numGender(races, FEMALE) == 0 ? FEMALE : numGender(races, MALE) == 0 ? MALE : null;
+    }
+
+    private static boolean getIsMixedClass(Vector<Race> races) {
+        return getMissingGender(races) == null ? true : false;
+    }
+
+    public static Vector<Race> selectBestMixedRacesForTeamId(int teamId, int numRacesToInclude, Vector<Race> races){
+
+        //will be populated with the extracted best races for the current TeamId
         Vector<Race> bestRaces = new Vector();
+
         Collections.sort(races, new RaceTimeComparator());
 
         Vector<Race> racesForTeamId = races.stream().filter(r -> r.getTeamID() == teamId).collect(Collectors.toCollection(Vector::new));
 
+        //Iterate over races per team to collect the fastest ones and store them in bestRaces
         racesForTeamId.stream().forEach(race -> {
-                    long numFemales = bestRaces.stream().filter(r -> classGenderMap.get(r.getClassID()) == FEMALE).count();
-                    boolean addOnlyFemale = numFemales == 0 && bestRaces.size() == numRacesToInclude-1 && classGenderMap.get(race.getClassID()) == FEMALE;
-                    boolean addFemaleOrMale = numFemales > 0 || bestRaces.size() < numTopRidersPerTeam-1;
+                    //Check if we should add another race, limit is specified by the const numRacesToInclude
                     boolean addAnother = bestRaces.size() < numRacesToInclude;
-                    if(addAnother && addFemaleOrMale || addOnlyFemale)
+                    //Any missing gender?
+                    Gender missingGender = getMissingGender(bestRaces);
+                    //Only add any gender if we are not at the last race to add, and we are not missing any gender to qualify for the "Mixed" criteria
+                    boolean addAnyGender = bestRaces.size() < numRacesToInclude-1 || getIsMixedClass(bestRaces);
+                    //If we are adding the last race, we need to check if previous races is missing out a gender, then we only add it if it is of the correct gender,
+                    //otherwise we continue the iteration to look for a race with the correct gender
+                    boolean addExplicitGender = bestRaces.size() == numRacesToInclude-1 && !getIsMixedClass(bestRaces) && classGenderMap.get(race.getClassID()) == missingGender;
+                    if(addAnother && addAnyGender || addExplicitGender)
                         bestRaces.add(race);
                 }
         );
-
-        if(bestRaces.size() < numRacesToInclude) throw new CompetitionCriteriaException("Could not find specified amount of riders for team number: " + teamId);
-
-        return bestRaces;
+        //Return an empty Vector if we don´t find enough races to qualify the team for mixed class
+        //Also, returns empty if a team is mixed but not have enough riders to meet the numRacesToInclude
+        return bestRaces.size() < numRacesToInclude ? new Vector() : bestRaces;
     }
 }
--- src/org/brosenius/test/MainTest.java
+++ src/org/brosenius/test/MainTest.java
@@ -7,6 +7,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.brosenius.domain.Gender.FEMALE;
+import static org.brosenius.domain.Gender.MALE;
 
 public class MainTest {
 
@@ -18,45 +19,81 @@ public class MainTest {
         Vector<Vector<Race>> winningTeamsWithBestRacesSortedAscending = RaceCalculatorUtil.calculateAndPrintResult(listOfIndividualResults);
 
         winningTeamsWithBestRacesSortedAscending.forEach(races -> {
-            /**
-             * Test for at least one female per winning team
-             */
-            assert(races.stream().filter(r -> RaceCalculatorUtil.classGenderMap.get(r.getClassID()) == FEMALE).collect(Collectors.toCollection(Vector::new)).size() > 0);
-            /**
-             * Test that all calculated teams have correct number of contestants
-             * @see RaceCalculatorUtil.numTopRidersPerTeam
-             */
-            assert(races.size() == RaceCalculatorUtil.numTopRidersPerTeam);
+
+            boolean qualifiedForMixedCompetition = races.size() > 0;
+
+            if(qualifiedForMixedCompetition) {
+                /**
+                 * Test for at least one female per mixed team
+                 */
+                assert (races.stream().filter(r -> RaceCalculatorUtil.classGenderMap.get(r.getClassID()) == FEMALE).collect(Collectors.toCollection(Vector::new)).size() > 0);
+                /**
+                 * Test for at least one male per mixed team
+                 */
+                assert (races.stream().filter(r -> RaceCalculatorUtil.classGenderMap.get(r.getClassID()) == MALE).collect(Collectors.toCollection(Vector::new)).size() > 0);
+                /**
+                 * Test that all calculated races have correct number of contestants
+                 * @see RaceCalculatorUtil.numTopRidersPerTeam
+                 */
+                assert (races.size() == RaceCalculatorUtil.numTopRidersPerTeam);
+            }
             /**
              * Test winners per team time calculation
              */
+            //teams qualified for mixed competition
             int team1Total = getTeamTotal(1);
             assert(team1Total == (44010 + 41450 + 31450));
             int team2Total = getTeamTotal(2);
             assert(team2Total == (22140 + 30760 + 44130));
             int team3Total = getTeamTotal(3);
             assert(team3Total == (32000 + 35980 + 38130));
+            //teams not qualified for mixed competition
+            int team4Total = getTeamTotal(4);
+            assert(team4Total == (0));
+            int team5Total = getTeamTotal(5);
+            assert(team5Total == (0));
+            int team6Total = getTeamTotal(6);
+            assert(team6Total == (0));
         });
     }
 
     private int getTeamTotal(int teamId){
-        return RaceCalculatorUtil.selectBestRacesForTeamIdIncludeMinimumOneFemale(teamId, RaceCalculatorUtil.numTopRidersPerTeam, listOfIndividualResults).stream().mapToInt(Race::getTotal).sum();
+        return RaceCalculatorUtil.selectBestMixedRacesForTeamId(teamId, RaceCalculatorUtil.numTopRidersPerTeam, listOfIndividualResults).stream().mapToInt(Race::getTotal).sum();
     }
 
     private void setupData() {
-        listOfIndividualResults.add(new Race(2,1,1,44010));
-        listOfIndividualResults.add(new Race(1,2,5,22140));
-        listOfIndividualResults.add(new Race(1,2,11,30760));
-        listOfIndividualResults.add(new Race(1,2,4,41010));
-        listOfIndividualResults.add(new Race(1,3,3,41410));
+        //Team 1, qualified for mixed, rank #3
         listOfIndividualResults.add(new Race(1,1,2,41450));
         listOfIndividualResults.add(new Race(1,1,2,31450));
         listOfIndividualResults.add(new Race(2,1,2,51450));
+        listOfIndividualResults.add(new Race(2,1,1,44010));
+        //Team 2,  qualified for mixed, rank #1
+        listOfIndividualResults.add(new Race(1,2,5,22140));
+        listOfIndividualResults.add(new Race(1,2,11,30760));
+        listOfIndividualResults.add(new Race(1,2,4,41010));
         listOfIndividualResults.add(new Race(2,2,6,44130));
+        //Team 3,  qualified for mixed, rank #2
+        listOfIndividualResults.add(new Race(1,3,3,41410));
         listOfIndividualResults.add(new Race(1,3,7,32000));
         listOfIndividualResults.add(new Race(2,3,9,35980));
         listOfIndividualResults.add(new Race(1,3,8,38130));
         listOfIndividualResults.add(new Race(1,3,10,39000));
+        //Team 4, too few contestants, excluded
+        listOfIndividualResults.add(new Race(2,4,9,35980));
+        listOfIndividualResults.add(new Race(1,4,8,38130));
+        //Team 5, only MALE, excluded
+        listOfIndividualResults.add(new Race(1,5,9,35980));
+        listOfIndividualResults.add(new Race(1,5,8,38130));
+        listOfIndividualResults.add(new Race(1,5,8,38130));
+        //Team 6, only FEMALE, excluded
+        listOfIndividualResults.add(new Race(2,6,9,35980));
+        listOfIndividualResults.add(new Race(2,6,8,38130));
+        listOfIndividualResults.add(new Race(2,6,8,38130));
+        //Team 7, same score as Team 1 qualified for mixed, rank shared #3
+        listOfIndividualResults.add(new Race(1,7,2,41450));
+        listOfIndividualResults.add(new Race(1,7,2,31450));
+        listOfIndividualResults.add(new Race(2,7,2,51450));
+        listOfIndividualResults.add(new Race(2,7,1,44010));
     }
 
 }
